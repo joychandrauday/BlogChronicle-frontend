ae2b0c0f5dcd963ac72067c653df70223137d52f
Joy Chandra Uday <joychandraud@gmail.com>
2024-12-20 20:38:46 +0600

    update update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [
-    // eslint-disable-next-line no-undef
-    require('daisyui'),
-  ],
-}
+    extend: {
 
+    }
+  },
+  plugins: [daisyui]
+};
